65baea5ccc0411c06b63a5e0c4617e8850236fe8
Valentina_PC <valentina.slaviero@edu-its.it>
2025-08-16 21:39:39 +0200

    tolti file per connessione al db inutili

--- app/models/user.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class User(db.Model):
-    __tablename__ = 'users'
-    
-    username = db.Column(db.String(80), primary_key=True)
-    password = db.Column(db.String(120), nullable=False)
--- app/services/user_service.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-
-class UserService:
-    @staticmethod
-    def authenticate(username, password):
-        return User.query.filter_by(username=username, password=password).first()
