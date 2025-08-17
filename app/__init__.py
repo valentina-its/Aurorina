a6aa3e8ad13295b88daab6a30ea53480276fa766
Valentina_PC <valentina.slaviero@edu-its.it>
2025-08-17 15:57:22 +0200

    impazzisco

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__,
-                 template_folder='templates')
+                 template_folder='app/templates')
     
     # Registra i blueprint
     from .controllers import tesoro_controller, sfida_controller, duck_controller
     app.register_blueprint(tesoro_controller.bp)
     app.register_blueprint(sfida_controller.bp)
-
     app.register_blueprint(duck_controller.bp)
     
     return app
